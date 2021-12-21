5174cbd33cc961b362db52e7764290b361666ddc
Dima Prudius <dimaprudius1995@gmail.com>
2021-12-21 22:26:57 +0200

    add database and model User, Article and enum Color

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,7 +1,30 @@
 package com.example.demo.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name")
     private String name;
+
+    @Column(name = "age")
     private int age;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "id")
+    private List<Article>articles = new ArrayList<>();
+
+//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "users")
+//    private List<Article>articles = new ArrayList<>();
 }
